b5ccf8c66681a31b8ba88c2c1e328a24d3ad0ced
Griffin O'Neill <gsoneill1003@gmail.com>
2021-09-20 13:39:42 -0400

    Fixed primitive serialization

--- src/entity/primitive.rs
+++ src/entity/primitive.rs
@@ -4,7 +4,24 @@ use std::collections::HashMap;
 use super::*;
 use serde::{Serialize, Serializer};
 
-// FIXME: This is untested and the component serialization is very likely flawed
+#[macro_export]
+macro_rules! primitive
+{
+    (
+        components: $(($name:expr, $cmp:expr)),*
+        mappings: $(($map_name:expr, $map_expr:expr)),*
+    ) => 
+    {
+        {
+            use std::collections::HashMap;
+            let mut components: HashMap<std::borrow::Cow<'static, str>, Box<dyn Component>> = HashMap::new();
+            $(components.insert($name.into(), Box::new($cmp));)*
+            let mut mappings = HashMap::new();
+            $(mappings.insert($map_name.into(), $map_expr.into());)*
+            PrimitiveReg::new(components, mappings)
+        }
+    }
+}
 
 #[derive(Serialize, Clone)]
 pub struct PrimitiveReg
@@ -18,11 +35,19 @@ impl PrimitiveReg
 {
     pub fn new
     (
-        default_components: HashMap<Cow<'static, str>, ComponentBox>, 
+        default_components: HashMap<Cow<'static, str>, Box<dyn Component>>, 
         mappings: HashMap<Cow<'static, str>, Cow<'static, str>>
     ) -> Self
     {
-        Self { default_components, mappings }
+        // This transmute is safe because ComponentBox is #[repr(transparent)]
+        Self { default_components: unsafe 
+        { 
+            std::mem::transmute::
+            <
+                HashMap<Cow<'static, str>, Box<dyn Component>>,
+                HashMap<Cow<'static, str>, ComponentBox>
+            >(default_components) 
+        }, mappings }
     }
 
     /// Register a primitive in aframe. Warning: Aframe must be initialized before this is called.
@@ -34,7 +59,7 @@ impl PrimitiveReg
 }
 
 #[repr(transparent)]
-pub struct ComponentBox(Box<dyn Component>);
+struct ComponentBox(Box<dyn Component>);
 
 impl Clone for ComponentBox
 {
